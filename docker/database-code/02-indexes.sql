df701d36e53e7c50f9e0b6445c4141bf2839d92b
Emilianovich <emiolafagbemi02@gmail.com>
2026-01-24 22:19:47 -0500

    feat: adding docker directory

---
+++ docker/database-code/02-indexes.sql
@@ -0,0 +1,6 @@
+CREATE INDEX idx_clients_id ON Clients(id);
+CREATE INDEX idx_clients_email ON Clients(email);
+CREATE INDEX idx_agents_id ON Agents(id);
+CREATE INDEX idx_tickets_id ON Tickets(id);
+CREATE INDEX idx_tickets_status_openedAt ON Tickets(status, "openedAt" DESC);
+CREATE INDEX idx_comments_ticketId ON Comments("ticketId");
