6bc1017037b178585b3bae5880b638815cd89cc6
Steven Ampie <ampiesteven24@gmail.com>
2026-01-27 00:46:11 -0500

    feat: added to CreateClientDto with validation rules for client data

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,1 +1,31 @@
-export class CreateClientDto {}
+import {
+  IsDateString,
+  IsEmail,
+  IsString,
+  MaxLength,
+  MinLength,
+} from "class-validator";
+
+export class CreateClientDto {
+  @IsString({ message: "Debe ser string" })
+  @MaxLength(15, {
+    message: "El username no puede ser mayor de 15 letras",
+  })
+  username: string;
+
+  @IsDateString(
+    {},
+    {
+      message: "La fecha no está en el formato adecuado YYYY-MM-DD ISO 8601",
+    },
+  )
+  dateOfBirth: string;
+
+  @IsString({ message: "Debe ser string" })
+  @IsEmail({}, { message: "Debe ser un email" })
+  email: string;
+
+  @IsString({ message: "La contraseña debe contener letras" })
+  @MinLength(8, { message: "La contraseña debe ser mayor a 8 letras" })
+  password: string;
+}
