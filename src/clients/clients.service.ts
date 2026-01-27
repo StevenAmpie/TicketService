370446d22d88deb64b40542b1cb730c92bcd4429
Steven Ampie <ampiesteven24@gmail.com>
2026-01-27 16:00:07 -0500

    feat: enhance ClientsService with exception handling for client creation and retrieval

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,26 +1,92 @@
-/*import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { CreateClientDto } from "./dto/create-client.dto";
 import { UpdateClientDto } from "./dto/update-client.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Client } from "./entities/client.entity";
+import { Not, Repository } from "typeorm";
+import { hashPassword } from "../helpers/hashPassword";
 
 @Injectable()
 export class ClientsService {
-  create(createClientDto: CreateClientDto) {
-    return "This action adds a new client";
+  constructor(
+    @InjectRepository(Client)
+    private readonly clientsRepository: Repository<Client>,
+  ) {}
+
+  async create(createClientDto: CreateClientDto) {
+    const exceptions: string[] = [];
+    const clientUsername = await this.clientsRepository.findOneBy({
+      username: createClientDto.username,
+    });
+    const clientEmail = await this.clientsRepository.findOneBy({
+      email: createClientDto.email,
+    });
+
+    if (clientUsername) {
+      exceptions.push("Ya existe un usuario con este nombre");
+    }
+
+    if (clientEmail) {
+      exceptions.push("El correo ingresado ya existe en el sistema");
+    }
+
+    if (exceptions.length >= 1) {
+      throw new HttpException({ detail: exceptions }, HttpStatus.CONFLICT);
+    }
+
+    // toDO = Save image in S3 instance, and then added public url.
+
+    createClientDto["password"] = await hashPassword(createClientDto.password);
+    createClientDto["picture"] = "ruta";
+    const newClient = this.clientsRepository.create(createClientDto);
+    return await this.clientsRepository.save(newClient);
   }
 
-  findAll() {
-    return `This action returns all clients`;
+  async findAll() {
+    const clients = await this.clientsRepository.find();
+
+    if (!clients) {
+      throw new HttpException("No se ha encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    return clients;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
+  async findOne(id: string) {
+    const client = await this.clientsRepository.findOneBy({ id });
+
+    if (!client) {
+      throw new HttpException("No se ha encontrado", HttpStatus.NOT_FOUND);
+    }
+    return client;
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
-  }
+  async update(id: string, updateClientDto: UpdateClientDto) {
+    const client = await this.clientsRepository.findOne({ where: { id } });
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
+    if (!client) {
+      throw new HttpException("No se ha encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    const userName = await this.clientsRepository
+      .createQueryBuilder("Clients")
+      .select("Clients.username")
+      .where({ username: updateClientDto.username })
+      .andWhere({ id: Not(id) })
+      .getOne();
+
+    if (userName) {
+      throw new HttpException(
+        "Ya existe un usuario con este nombre",
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    // toDO = Save image in S3 instance, and then added public url.
+    updateClientDto["picture"] = "ruta";
+
+    this.clientsRepository.merge(client, updateClientDto);
+
+    return await this.clientsRepository.save(client);
   }
-}*/
+}
