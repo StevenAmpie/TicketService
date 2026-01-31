2149b0884a7d85e86c254e9a87c0e03879610e10
Emilianovich <emiolafagbemi02@gmail.com>
2026-01-31 02:15:01 -0500

    feat: added delete and upload method to S3 Service Class

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from "@nestjs/common";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+  DeleteObjectCommand,
+  PutObjectCommand,
+  waitUntilObjectNotExists,
+} from "@aws-sdk/client-s3";
 import { S3Bucket } from "./s3-bucket";
 import { OurS3Client } from "./our-s3-Client";
 
-type UploadTypes = {
+type UploadType = {
   key: string;
   buffer: Buffer;
   contentType: string;
@@ -15,7 +19,7 @@ export class S3Service {
     private s3BucketClient: S3Bucket,
     private s3ClientService: OurS3Client,
   ) {}
-  async upload({ key, buffer, contentType }: UploadTypes) {
+  async upload({ key, buffer, contentType }: UploadType) {
     const newUpload = new PutObjectCommand({
       Bucket: this.s3BucketClient.bucket,
       Key: key,
@@ -26,7 +30,37 @@ export class S3Service {
       await this.s3ClientService.send(newUpload);
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (e) {
-      return false;
+      return null;
+    }
+    return true;
+  }
+  async delete(key: string) {
+    try {
+      await this.s3ClientService.send(
+        new DeleteObjectCommand({
+          Bucket: this.s3BucketClient.bucket,
+          Key: key,
+        }),
+      );
+      await waitUntilObjectNotExists(
+        { maxWaitTime: 30, client: this.s3ClientService },
+        { Bucket: this.s3BucketClient.bucket, Key: key },
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (e) {
+      return null;
+    }
+    return true;
+  }
+
+  async updateFile({ key, buffer, contentType }: UploadType) {
+    const wasDeleted = await this.delete(key);
+    if (!wasDeleted) {
+      return null;
+    }
+    const wasUploaded = await this.upload({ key, buffer, contentType });
+    if (!wasUploaded) {
+      return null;
     }
     return true;
   }
