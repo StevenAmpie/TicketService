c2842f20a9642130f3bfd701f9721b5de57baafe
Steven Ampie <ampiesteven24@gmail.com>
2026-01-30 23:59:24 -0500

    feat: update AuthModule to include Client entity and JWT module registration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,18 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { RefreshToken } from "./entities/refresh-token.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Client } from "src/clients/entities/client.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { LocalStrategy } from "./strategies/local.strategie";
+import { ConfigService } from "@nestjs/config";
+import { JwtStrategy } from "./strategies/jwt.strategie";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RefreshToken])],
+  imports: [
+    TypeOrmModule.forFeature([RefreshToken, Client]),
+    JwtModule.register({}),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy, ConfigService],
 })
 export class AuthModule {}
