b7257e253b022c77480c344b6873bc6977ec6ce7
Steven Ampie <ampiesteven24@gmail.com>
2026-01-30 23:21:49 -0500

    feat: implement JWT and Local authentication strategies

---
+++ src/auth/strategies/jwt.strategie.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow("JWT_SECRET_KEY"),
+    });
+  }
+
+  validate(payload: { sub: string; username: string; role: string }) {
+    return payload;
+  }
+}
