d38de557a9d3205798e045b32e2fa102bea24905
Steven Ampie <ampiesteven24@gmail.com>
2026-02-01 05:42:40 -0500

    feat: enhance authentication endpoints with JWT handling and cookie management

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,21 @@
-import { Body, Controller, Delete, Post, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  ParseUUIDPipe,
+  Post,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
 import { LoginDto } from "./dto/login-dto";
 import { JwtAuthGuard } from "./guards/jwt-auth.guard";
+import type { Response } from "express";
+import { Cookies } from "src/decorators/cookies.decorator";
+import { CurrentUser } from "src/decorators/getCurrentUser.decorator";
+import { JwtDto } from "./dto/jwt-dto";
 
 @Controller()
 export class AuthController {
@@ -14,18 +27,43 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post("login")
-  login(@Body() body: LoginDto) {
-    return this.authService.login(body);
+  async login(
+    @CurrentUser() loginData: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken, refreshToken } =
+      await this.authService.login(loginData);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      path: "/auth",
+    });
+
+    return { accessToken };
   }
 
-  @Post("refresh")
-  refreshToken(@Body() body: { userId: string; token: string }) {
-    return this.authService.refreshToken(body.userId, body.token);
+  @Post("auth/refresh")
+  async refreshToken(
+    @Body("id", new ParseUUIDPipe()) userId: string,
+    @Cookies("refreshToken") refreshToken: string,
+  ) {
+    if (!refreshToken) {
+      throw new UnauthorizedException("Token inválido");
+    }
+    return await this.authService.refreshToken(userId, refreshToken);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Delete("logout")
-  logout(@Body() body: { userId: string; token: string }) {
-    return this.authService.logout(body.userId, body.token);
+  @Delete("auth/logout")
+  async logout(
+    @CurrentUser() jwt: JwtDto,
+    @Cookies("refreshToken") refreshToken: string,
+  ) {
+    if (!refreshToken) {
+      throw new UnauthorizedException("Token inválido");
+    }
+    return await this.authService.logout(jwt.sub, refreshToken);
   }
 }
