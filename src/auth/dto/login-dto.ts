d8479beb03318f74b363f86dcf841311afea5142
Steven Ampie <ampiesteven24@gmail.com>
2026-02-01 05:38:13 -0500

    feat: create JwtDto class and update LoginDto with UUID for id

--- src/auth/dto/login-dto.ts
+++ src/auth/dto/login-dto.ts
@@ -1,10 +1,19 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { IsEmail, IsString, IsUUID, MinLength } from "class-validator";
 
 export class LoginDto {
+  @IsUUID()
+  id: string;
+
   @IsEmail()
   email: string;
 
   @IsString({ message: "La contraseña debe contener letras" })
   @MinLength(8, { message: "La contraseña debe ser mayor a 8 letras" })
   password: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  role: string;
 }
