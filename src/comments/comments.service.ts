62da6494081aa7e462a6b6f3d7788a28e1c53bd0
Steven Ampie <ampiesteven24@gmail.com>
2026-02-03 18:58:16 -0500

    feat: implement authentication and authorization in Comments module; enhance findAllByTicketId method with user role checks

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -2,24 +2,58 @@ import {
   BadRequestException,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from "@nestjs/common";
 import { CreateCommentDto } from "./dto/create-comment.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Comment } from "./entities/comment.entity";
+import { JwtDto } from "src/auth/dto/jwt-dto";
+import { Ticket } from "src/tickets/entities/ticket.entity";
+import { TicketCase } from "src/tickets-cases/entities/ticket-case.entity";
 
 @Injectable()
 export class CommentsService {
   constructor(
     @InjectRepository(Comment)
     private readonly commentRepository: Repository<Comment>,
+
+    @InjectRepository(Ticket)
+    private readonly ticketRepository: Repository<Ticket>,
+
+    @InjectRepository(TicketCase)
+    private readonly ticketCaseRepository: Repository<TicketCase>,
   ) {}
 
-  async findAllByTicketId(id: string) {
+  async findAllByTicketId(ticketId: string, user: JwtDto) {
+    if (user.role === "client") {
+      const verifiedClient = await this.ticketRepository.findOne({
+        where: { id: ticketId, clientId: user.sub },
+      });
+
+      if (!verifiedClient) {
+        throw new UnauthorizedException(
+          "No tienes permiso para ver este recurso",
+        );
+      }
+    }
+
+    if (user.role === "agent") {
+      const verifiedAgent = await this.ticketCaseRepository.findOne({
+        where: { ticketId: ticketId, agentId: user.sub },
+      });
+
+      if (!verifiedAgent) {
+        throw new UnauthorizedException(
+          "No tienes permiso para ver este recurso",
+        );
+      }
+    }
+
     const comments = await this.commentRepository
       .createQueryBuilder("comments")
       .select(["comments.content", "comments.role", "comments.publishedAt"])
-      .where({ ticketId: id })
+      .where({ ticketId: ticketId })
       .orderBy("comments.publishedAt", "ASC")
       .getMany();
 
