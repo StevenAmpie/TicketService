94982c2744bf6d1049d918da23426df5a868490d
Emilianovich <emiolafagbemi02@gmail.com>
2026-02-02 20:25:53 -0500

    refactor: changed the hardcoded Sentry dsn to reading the env variable

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,8 +1,9 @@
 import * as Sentry from "@sentry/nestjs";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import "dotenv/config";
 
 Sentry.init({
-  dsn: "https://bb831b0670c254f008b48397ed097311@o4510817020870656.ingest.us.sentry.io/4510817024409600",
+  dsn: process.env.SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0, // discuss this
   sendDefaultPii: true,
