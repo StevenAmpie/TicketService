c797fb2828b8c3cf8b633414c95d8601b96a8ca2
Steven Ampie <ampiesteven24@gmail.com>
2026-02-03 18:58:16 -0500

    feat: implement authentication and authorization in Comments module; enhance findAllByTicketId method with user role checks

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,16 +1,21 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { CommentsService } from "./comments.service";
 import { CreateCommentDto } from "./dto/create-comment.dto";
+import { CurrentUser } from "src/decorators/getCurrentUser.decorator";
+import { JwtDto } from "src/auth/dto/jwt-dto";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 
 @Controller()
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get("tickets/:uuid/comments")
-  async findAll(@Param("uuid") id: string) {
-    return await this.commentsService.findAllByTicketId(id);
+  async findAll(@CurrentUser() user: JwtDto, @Param("uuid") ticketId: string) {
+    return await this.commentsService.findAllByTicketId(ticketId, user);
   }
 
+  // ToDo: added authorization in WebSockets
   @Post("comments")
   async create(@Body() comment: CreateCommentDto) {
     return await this.commentsService.create(comment);
